0b048c3eac86110f86aa24eccff0d40056f7d527
DDPan147 <ddgarpan147@gmail.com>
2025-02-19 14:45:06 +0100

    Actualizacion Minijuego 8 Ahora mismo funciona con bugs

--- Project_Lighthouse/Assets/Scripts/Minijuegos/Minijuego8/RotarCandado.cs
+++ Project_Lighthouse/Assets/Scripts/Minijuegos/Minijuego8/RotarCandado.cs
@@ -7,6 +7,7 @@ public class RotarCandado : MonoBehaviour
     private Camera cam;
     public GameObject canica, candado;
     private float rotation;
+    public bool canRotate = true;
     void Start()
     {
         cam = Camera.main;
@@ -22,13 +23,36 @@ public class RotarCandado : MonoBehaviour
 
             if (Physics.Raycast(ray.origin, ray.direction * 10, out hit))
             {
-                if (hit.collider.gameObject.CompareTag("Tuberia"))
+                if (hit.collider.gameObject.CompareTag("Tuberia") && canRotate)
                 { 
+                    canRotate = false;
                     rotation += -90;
-                    candado.transform.DORotate(new Vector3(candado.transform.localEulerAngles.x,candado.transform.localEulerAngles.y, rotation), 0.5f, RotateMode.Fast).OnComplete(() => canica.GetComponent<Pelota>().MoveMarble());
+                    transform.DORotate(new Vector3(candado.transform.localEulerAngles.x,candado.transform.localEulerAngles.y, rotation), 0.5f, RotateMode.Fast).OnComplete(() => canica.GetComponent<Pelota>().MoveMarble());
+                }
+                if (hit.collider.gameObject.CompareTag("Tuberia_Entrada") && canRotate)
+                { 
+                    canRotate = false;
+                    rotation += 90;
+                    transform.DORotate(new Vector3(candado.transform.localEulerAngles.x,candado.transform.localEulerAngles.y, rotation), 0.5f, RotateMode.Fast).OnComplete(() => canica.GetComponent<Pelota>().MoveMarble());
                 }
             }
         }
         
+    }
+
+    public void Win()
+    {
+        Debug.Log("Has Ganado");
+        candado.transform.DORotate(new Vector3(0, 90, 0), 1.5f, RotateMode.Fast);
+    }
+
+    public void CanRotate()
+    {
+        canRotate = true;
+    }
+
+    public void CantRotate()
+    {
+        canRotate = false;
     }
 }
