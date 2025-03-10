743e1c41952cfef8b66c5485f82c5d9dc7771b12
DDPan147 <ddgarpan147@gmail.com>
2025-03-10 14:40:29 +0100

    Minijuego 8 y 9 Update
    Aplicado Feedback Minijuego 8 y el 9 en proceso

--- Project_Lighthouse/Assets/Scripts/Minijuegos/Minijuego9/Nuevo/Cloud.cs
+++ Project_Lighthouse/Assets/Scripts/Minijuegos/Minijuego9/Nuevo/Cloud.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using TMPro;
 using UnityEngine;
 
 public class Cloud : MonoBehaviour
@@ -13,23 +14,58 @@ public class Cloud : MonoBehaviour
         {
             if(value == true)
             {
-               gameObject.SetActive(false);
+                FadeOut();
             }
         }
     }
     [HideInInspector] public bool isInvincible = false;
     [HideInInspector] public float timeLighting;
     [HideInInspector] public Vector3 initialScale;
+    private Renderer mainRenderer;
+    private Material mainMat;
     public int points;
+    public TMP_Text pointsText;
+    public float timeFadeIn;
+    private void Awake()
+    {
+        mainRenderer = GetComponent<Renderer>();
+        mainMat = mainRenderer.material;
+    }
 
     void Start()
     {
+        pointsText.enabled = false;
         initialScale = transform.localScale;
+        FadeIn();
     }
 
-
-    void Update()
+    void TextFadeOut()
     {
+        pointsText.DOFade(0f, 0.4f).OnComplete(() =>
+        {
+            gameObject.SetActive(false);
+        });
+    }
 
+    void FadeOut()
+    {
+        mainMat.DOFade(0f, 0.5f).OnComplete(() =>
+        {
+            pointsText.enabled = true;
+            mainRenderer.enabled = false;
+            TextFadeOut();
+        });
+
+    }
+
+    void FadeIn()
+    {
+        mainMat.color = new Color(mainMat.color.r, mainMat.color.g, mainMat.color.b, 0);
+        mainMat.DOFade(1, timeFadeIn);
+    }
+
+    void InitialMovement()
+    {
+        // Movimiento Inicial
     }
 }
