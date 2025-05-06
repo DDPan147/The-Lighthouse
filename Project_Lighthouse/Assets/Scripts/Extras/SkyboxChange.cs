96be7a8fdaaa86f66099f2eed7ab6deed80b160e
DDPan147 <ddgarpan147@gmail.com>
2025-05-06 14:04:25 +0200

    Update Skybox
    Añadido un script que cambia de skybox y los colores del mar

---
+++ Project_Lighthouse/Assets/Scripts/Extras/SkyboxChange.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+
+public class SkyboxChange : MonoBehaviour
+{
+    public Color[] dayWaterColour;
+    public Color[] nightWaterColour;
+    public Material daySkybox; 
+    public Material nightSkybox; 
+    private Material mat;
+    private Shader waterShader;
+    private void Awake()
+    {
+        mat = GetComponent<Renderer>().material;
+        
+        /*Debug.Log(mat.shader.name);
+        waterShader = mat.shader;
+        waterShader.*/
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            SetNightColours();
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            SetDayColours();
+        }
+    }
+
+    void SetDayColours()
+    {
+        mat.SetColor("_DeepWaterColor", dayWaterColour[0]);
+        mat.SetColor("_WaterColor", dayWaterColour[1]);
+        mat.SetColor("_ShallowWaterColor", dayWaterColour[2]);
+        RenderSettings.skybox = daySkybox;
+    }
+    void SetNightColours()
+    {
+        mat.SetColor("_DeepWaterColor", nightWaterColour[0]);
+        mat.SetColor("_WaterColor", nightWaterColour[1]);
+        mat.SetColor("_ShallowWaterColor", nightWaterColour[2]);
+        RenderSettings.skybox = nightSkybox;
+    }
+
+}
