caee4745952d708aabb85931b373f791e8dbf883
Dorabyte-Dev <dorabyte@gmail.com>
2025-05-28 09:34:08 +0200

    Luna Fantasma WIP

--- Project_Lighthouse/Assets/Scripts/Minijuegos/Minijuego5/LunaFantasma.cs
+++ Project_Lighthouse/Assets/Scripts/Minijuegos/Minijuego5/LunaFantasma.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
+using UnityEngine.Splines;
 
 public class LunaFantasma : MonoBehaviour
 {
+    public SplineContainer spline;
+    private float distancePercentage;
+
+    public bool activated;
+
+    [SerializeField]private float speed;
     void Start()
     {
         
@@ -9,6 +16,14 @@ public class LunaFantasma : MonoBehaviour
 
     void Update()
     {
-        
+        if (activated)
+        {
+            distancePercentage += speed * Time.deltaTime;
+
+            Vector3 currentPosition = spline.EvaluatePosition(distancePercentage);
+            transform.position = currentPosition;
+
+            if(distancePercentage > 1f)
+        }
     }
 }
