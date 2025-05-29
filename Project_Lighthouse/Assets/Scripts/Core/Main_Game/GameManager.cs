4dd555ca650a0b49892319c7d022b832ba98a4c0
Dorabyte-Dev <dorabyte@gmail.com>
2025-05-29 18:37:46 +0200

    Puliendo Detalles. Ya no se que poner xd
    Animacion del principio del abuelo, cambios en algunas splines de movimiento, arreglos y ajustes de posiciones, etc...

--- Project_Lighthouse/Assets/Scripts/Core/Main_Game/GameManager.cs
+++ Project_Lighthouse/Assets/Scripts/Core/Main_Game/GameManager.cs
@@ -18,6 +18,7 @@ public class GameManager : MonoBehaviour
 
 
     public Material highlightMat;
+    public Material highlightMinigameMat;
 
     //[Header("SoundManager")]
     private SoundManager sm;
